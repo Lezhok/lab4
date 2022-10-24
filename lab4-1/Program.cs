423ff4ea592dd1912626836ea813b81939d31953
Lezhok <112374444+Lezhok@users.noreply.github.com>
2022-10-24 17:37:45 +0300

    Add files via upload

---
+++ lab4-1/Program.cs
@@ -0,0 +1,47 @@
+﻿internal class Printer
+{
+    public virtual void Print(string value)
+    {
+        Console.BackgroundColor = ConsoleColor.Green;
+        Console.WriteLine(value);
+    }
+
+}
+
+internal class PrinterYellow : Printer
+{
+    public override void Print(string value)
+    {
+        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.WriteLine(value);
+    }
+}
+
+internal class PrinterBlue : Printer
+{
+    public new void Print(string value)
+    {
+        Console.BackgroundColor = ConsoleColor.Blue;
+        Console.WriteLine(value);
+    }
+
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var arr = new Printer[3];
+        arr[0] = new Printer();
+        arr[1] = new PrinterYellow();
+        arr[2] = new PrinterBlue();
+
+        foreach (Printer r in arr)
+        {
+            r.Print("Hello");
+        }
+
+        ((PrinterBlue)arr[2]).Print("ad");
+    }
+
+}
---
+++ lab4-2/Program.cs
@@ -0,0 +1,95 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Pupil pupil1 = new Pupil("Олександр");
+        Pupil pupil2 = new BadPupil("Владислав");
+        Pupil pupil3 = new GoodPupil("Дар'я");
+        Pupil pupil4 = new ExellentPupil("Олег");
+
+        var firstClassRoom = new ClassRoom(pupil1);
+        var secondClassRoom = new ClassRoom(pupil1, pupil2);
+        var thirdClassRomm = new ClassRoom(pupil1, pupil2, pupil3);
+        var forthClassRomm = new ClassRoom(pupil1, pupil2, pupil3, pupil4);
+        forthClassRomm.GetPupilsRead();
+
+        Console.ReadLine();
+    }
+}
+
+public class Pupil
+{
+    public string PupilName { get; private set; }
+    public Pupil(string name)
+    {
+        PupilName = name;
+    }
+
+    private string GetPupilNameAndStatus()
+    {
+        return String.Format("{0} {1}", GetType().Name, PupilName);
+    }
+
+    public virtual void Read()
+    {
+        Console.WriteLine("{0} {1}", GetPupilNameAndStatus(), "Reading");
+    }
+
+    public virtual void Write()
+    {
+        Console.WriteLine("{0} {1}", GetPupilNameAndStatus(), "Writting");
+    }
+
+    public virtual void Relax()
+    {
+        Console.WriteLine("{0} {1}", GetPupilNameAndStatus(), "Relaxing");
+    }
+
+    public virtual void Study()
+    {
+        Console.WriteLine("{0} {1}", GetPupilNameAndStatus(), "Studying");
+    }
+}
+
+public class ExellentPupil : Pupil
+{
+    public ExellentPupil(string name)
+        : base(name)
+    { }
+    public override void Read()
+    {
+        Console.WriteLine("{0} reads more than usual pupil", PupilName);
+    }
+}
+
+public class GoodPupil : Pupil
+{
+    public GoodPupil(string name)
+        : base(name)
+    { }
+}
+
+public class BadPupil : Pupil
+{
+    public BadPupil(string name)
+        : base(name)
+    { }
+}
+
+public class ClassRoom
+{
+    public readonly List<Pupil> Pupils = new List<Pupil>();
+
+    public ClassRoom(params Pupil[] pupils)
+    {
+        Pupils.AddRange(pupils);
+    }
+
+    public void GetPupilsRead()
+    {
+        foreach (var pupil in Pupils)
+        {
+            pupil.Read();
+        }
+    }
+}
